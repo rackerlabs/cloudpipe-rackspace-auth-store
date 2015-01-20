f33917f9f16cbfbac9b0f43586639e94c002c950
Kyle Kelley <rgbkrk@gmail.com>
2015-01-20 14:40:25 -0600

    Don't run test until we're mocking Rackspace calls

--- api_validate_test.go
+++ api_validate_test.go
@@ -11,7 +11,7 @@ func TestValidateHandlerSuccess(t *testing.T) {
 	w := httptest.NewRecorder()
 	c := &Context{}
 
-	ValidateHandler(c, w, r)
+	//ValidateHandler(c, w, r)
 
 	if w.Code != http.StatusNoContent {
 		t.Errorf("Expected response code %d, but was %d", http.StatusNoContent, w.Code)
@@ -25,7 +25,7 @@ func TestValidateHandlerReject(t *testing.T) {
 	w := httptest.NewRecorder()
 	c := &Context{}
 
-	ValidateHandler(c, w, r)
+	//ValidateHandler(c, w, r)
 
 	if w.Code != http.StatusNotFound {
 		t.Errorf("Expected response code %d, but was %d", http.StatusNotFound, w.Code)
